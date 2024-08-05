b4a65ad89360e15260fa66c01e62f17456249406
Ben Galewsky <ben@peartreestudio.net>
2024-08-05 11:01:26 -0500

    Improve usage with MLFlow

--- scripts/simulation.sh
+++ scripts/simulation.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
-module load gcc/11.2.0
-module load cmake openmpi/4.0.5-intel-18.0
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/projects/qmchamm/shared/scott_builds/libtorch/lib"
-module list
 echo $LD_LIBRARY_PATH
 export OMP_NUM_THREADS=12
 export MKL_NUM_THREADS=12
 
-export SCRIPTS_DIR=../scripts
+export SCRIPTS_DIR=../../scripts
 
 while getopts :m:s:p:t:n:v: flag
 do
     case "${flag}" in
         m) model=${OPTARG};;
         s) steps=${OPTARG};;
-        p) pres="-p ${OPTARG}";;
+        p) presure_option="-p ${OPTARG}";pressure=${OPTARG};;
         t) temp="-t ${OPTARG}";;
         v) velocity="-v ${OPTARG}";;
         n) nbeads="-n ${OPTARG}";;
@@ -22,10 +19,8 @@ do
 done
 
 echo "model: $model";
-
-mkdir -p $MLFLOW_RUN_ID
-cd $MLFLOW_RUN_ID
-
+cd p$pressure
+echo "Cd into $(realpath .)"
 node=$(hostname)
 node=${node%%.*}
 cur_time=$(date +%Y%m%d%H%M%S)
@@ -33,12 +28,12 @@ IPI=$(which i-pi)
 lmp=/projects/qmchamm/shared/scott_builds/lammps/build_misc/bin/lmp
 if [ -f simulation.restart ]
 then
-	python $SCRIPTS_DIR/edit_xml.py -i ../input.restart.xml -o input.xml $temp -a ${node}${cur_time} $pres $nbeads
+	python $SCRIPTS_DIR/edit_xml.py -i ../../input.restart.xml -o input.xml $temp -a ${node}${cur_time} $presure_option $nbeads
 	python -u $IPI input.xml > log.ipi.txt &
 
 	sleep 6
 else
-	python $SCRIPTS_DIR/edit_xml.py -i ../input.xml -o input.xml $velocity $temp -a ${node}${cur_time} $pres $nbeads
+	python $SCRIPTS_DIR/edit_xml.py -i ../../input.xml -o input.xml $velocity $temp -a ${node}${cur_time} $presure_option $nbeads
 	python -u $IPI input.xml > log.ipi.txt &
 
 	sleep 6
