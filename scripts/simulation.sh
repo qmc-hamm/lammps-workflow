5e018a2cb56c2ae1fca2be6b64e34f6b1c8acc32
Ben Galewsky <ben@peartreestudio.net>
2024-08-06 10:45:57 -0500

    Add simulation to run script

--- scripts/simulation.sh
+++ scripts/simulation.sh
@@ -11,41 +11,44 @@ cur_time=$(date +%Y%m%d%H%M%S)
 IPI=$(which i-pi)
 lmp=/projects/bcqo/shubhanggoswami/lammps/build/bin/lmp
 
-export SCRIPTS_DIR=../../scripts
+export SCRIPTS_DIR=$(realpath ../scripts)
 
 while getopts :m:s:p:t:n:v: flag
 do
     case "${flag}" in
-        m) model=${OPTARG};;
         s) steps=${OPTARG};;
         p) presure_option="-p ${OPTARG}";pressure=${OPTARG};;
-        t) temp="-t ${OPTARG}";;
+        t) temp=${OPTARG};;
         v) velocity="-v ${OPTARG}";;
         n) nbeads="-n ${OPTARG}";;
         c) case=${OPTARG};;
     esac
 done
-
-echo "model: $model";
+pot_dir=$(realpath .)
 cd p$pressure
-echo "Cd into $(realpath .)"
-node=$(hostname)
-node=${node%%.*}
-cur_time=$(date +%Y%m%d%H%M%S)
-IPI=$(which i-pi)
-lmp=/projects/qmchamm/shared/scott_builds/lammps/build_misc/bin/lmp
+echo "cd into $(realpath .)"
+
+mkdir -p cache/$case/$temp/analysis
+cp data.txt cache/$case/$temp
+cp init.xyz cache/$case/$temp
+cd cache/$case/$temp
+
 if [ -f simulation.restart ]
 then
-	python $SCRIPTS_DIR/edit_xml.py -i ../../input.restart.xml -o input.xml $temp -a ${node}${cur_time} $presure_option $nbeads
+	python $SCRIPTS_DIR/edit_xml.py -i ../../../../input.restart.xml -o input.xml -t $temp -a ${node}${cur_time} $presure_option $nbeads
 	python -u $IPI input.xml > log.ipi.txt &
 
-	sleep 6
+	sleep 60
+
+	$lmp -v address ${node}${cur_time} \
+	-v pot_dir $pot_dir \
+	-in ../in.mace.$case.txt &
 else
-	python $SCRIPTS_DIR/edit_xml.py -i ../../input.xml -o input.xml $velocity $temp -a ${node}${cur_time} $presure_option $nbeads
+	python $SCRIPTS_DIR/edit_xml.py -i ../../../../input.xml -o input.xml $velocity -t $temp -a ${node}${cur_time} $presure_option $nbeads
 	python -u $IPI input.xml > log.ipi.txt &
 
 	sleep 60
 	$lmp -v address ${node}${cur_time} \
-	-v pot_dir $(realpath ..) \
+	-v pot_dir $pot_dir \
 	-in ../in.mace.$case.txt &
 fi
--- simulation_run.py
+++ simulation_run.py
@@ -31,11 +31,41 @@ def gen_xyz(parent_run_id: str, experiment_id: int, pressure: int):
         experiment_id=experiment_id
     )
 
+def run_lammps(parent_run_id: str, experiment_id: int, backend_config: str,
+    pressure: int, temperature: int, nbeads: int, iterations: int, iterations_per_job: int):
+
+    assert iterations % iterations_per_job == 0 # Ensure we can divide the iterations into evenly sized jobs
+    num_jobs = iterations // iterations_per_job
+
+    p = mlflow.projects.run(
+        uri=os.path.dirname(os.path.realpath(__file__)),
+        entry_point="simulation",
+        run_name=f"Run LAMMPS Simulation for Pressure {pressure} and Temperature {temperature}",
+        parameters={
+            "parent_run_id": parent_run_id,
+            "pressure": pressure,
+            "temperature": temperature,
+            "nbeads": nbeads,
+            "iterations": iterations_per_job,
+            "sequential_workers": num_jobs,
+            "case": "QMC"
+        },
+        experiment_id=experiment_id,
+        synchronous=False,
+        backend="slurm",
+        backend_config=backend_config
+    )
 @click.command(help="Perform LAMMPS Simulation with Mace Model.")
 @click.option("--model-uri", type=click.STRING,  help="Repository URI for Mace Model.", required=True)
 @click.option("--pressures", type=click.STRING, help="Pressures to simulate.", required=True)
-def run(model_uri: str, pressures: str):
+@click.option("--temperatures", type=click.STRING, help="Temperatures to simulate.", required=True)
+@click.option("--nbeads", type=click.INT, help="Number of beads.", required=False, default=16)
+@click.option("--iterations", type=click.INT, help="Total number of iterations.", required=False, default=9000)
+@click.option("--iterations-per-job", type=click.INT, help="Number of iterations that can be achieved in a single job", required=False, default=1000)
+def run(model_uri: str, pressures: str, temperatures: str, nbeads: int,
+    iterations, iterations_per_job):
     pressure_list = [int(pressure) for pressure in pressures.split(",")]
+    temperature_list = [int(temperature) for temperature in temperatures.split(",")]
     provided_run_id = os.environ.get("MLFLOW_RUN_ID", None)
     with mlflow.start_run(run_id=provided_run_id) as run:
         if not provided_run_id:
@@ -49,6 +79,13 @@ def run(model_uri: str, pressures: str):
         # Create directories for each pressure and populate with position files
         for pressure in pressure_list:
             gen_xyz(provided_run_id, experiment_id, pressure)
+
+        # Now run LAMMPS simulations over each pressure and temperature
+        for pressure in pressure_list:
+            for temperature in temperature_list:
+                run_lammps(provided_run_id, experiment_id, "cpu-delta.json",
+                    pressure, temperature, nbeads, iterations, iterations_per_job)
+
 
 
 if __name__ == "__main__":
